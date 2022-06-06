d95a7b16ac669e635ff67d5ed7c7c815d3ff8a1f
Nisarg Dave <extrastuff58@yahoo.com>
2022-06-06 14:23:01 +0800

    Created skeleton for feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 
 const Home: NextPage = () => {
@@ -10,10 +11,9 @@ const Home: NextPage = () => {
         <title>Twitter 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello World</h1>
       <main>
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </main>
     </div>
